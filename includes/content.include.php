25fe507b092162e342513afdb235112565058a30
Rodolfo <rodolfo.novak@gmail.com>
2012-08-23 14:54:44 -0400

    spans

--- includes/content.include.php
+++ includes/content.include.php
@@ -19,12 +19,12 @@
 				  <h1>About me <small> Some Acronyms UX, GUI, IA, AD </small></h1>
 	</div>
 	<div class="row">
-		<div class="span8">
+		<div class="span7">
 				  <p>Rodolfo Novak hails from São Paulo, Brazil and calls Toronto, Canada home, where he lives and breathes product development, entrepreneurship and design. Rodolfo is well versed in the start-up world and focused on improving product adoption by creating more meaningful consumer experiences.  He has integrated that focus into the latest ventures that he co-founded, Ripe Apps in the mobile applications space and Quandl, a disruptive search engine and platform.</p>
 				  <p>He is fluent in English, Brazilian Portuguese and Spanish.</p>
 				  <h4>See some of the things people are saying <a href="http://www.linkedin.com/profile/view?id=13923042&amp;authType=name&amp;authToken=GG3f#recommendations" onclick="javascript:_gaq.push(['_trackEvent','outbound-article','http://www.linkedin.com']);">about him.</a></h4>
 				  </div>
-			<div class="span4">
+			<div class="span3">
 	<img src="http://m2.licdn.com/mpr/mpr/shrink_150_150/p/2/000/192/18a/25326cb.jpg"  class="img-polaroid">
 			</div>		  
 		 </div>
