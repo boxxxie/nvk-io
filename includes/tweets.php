22e298d0ee235e1138859a8caa8917ca313fc995
Rodolfo <rodolfo.novak@gmail.com>
2012-08-27 11:34:08 -0400

    tweets

---
+++ includes/tweets.php
@@ -0,0 +1,15 @@
+<div class="tweets">
+	<script src="path/to/chirp.min.js"></script>
+	<script>Chirp({
+	  user: 'nvk', //Twitter username
+	  max: 10, //Maximum number of tweets to show 
+	  count: 100, //Total tweets to retrieve
+	  retweets: true, //Show/Don't show retweets
+	  replies: false,  //Show/Don't show replies
+	  cacheExpire: 1000 * 60 * 2 //Number of milliseconds to cache tweets
+	})</script>
+<br/>
+</div>
+
+
+
